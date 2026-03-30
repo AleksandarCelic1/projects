da28ff248b8faa000875de70c6bc1ef5848654cf
Aleksandar Celic <acocelic24@gmail.com>
2026-03-30 19:49:34 +0200

    made initial Diagram and classes according to it, started making Game class and how does it handle screen resolution

---
+++ rpg/classes_/include_/game.hpp
@@ -0,0 +1,18 @@
+#include <SDL.h>
+
+typedef struct
+{
+  int screen_height_;
+  int screen_width_;
+  bool fullscreen_;
+  int scaling_factor_;
+} GameSettings;
+
+
+class Game
+{
+  private:
+
+  public:
+
+};
